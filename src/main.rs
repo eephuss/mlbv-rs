00c43b3cc7d70806586127ef9d18ce4aae61e70d
Tom Cole <16390297+eephuss@users.noreply.github.com>
2025-10-13 16:05:52 -0400

    use stages

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 mod config;
 mod session;
 
+use crate::config::AppConfig;
 use crate::session::MlbSession;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut session = MlbSession::new()?;
-    session.authenticate().await?;
+    let cfg = AppConfig::load()?;
+
+    let session = MlbSession::new()?
+        .authenticate(&cfg.credentials.username, &cfg.credentials.password)
+        .await?
+        .fetch_okta_code()
+        .await?
+        .exchange_tokens()
+        .await?;
+
+    println!("{:?}", session.state.okta_tokens);
 
     Ok(())
 }
--- src/session.rs
+++ src/session.rs
@@ -1,4 +1,3 @@
-use crate::config::AppConfig;
 use anyhow::Ok;
 use oauth2::{CsrfToken, PkceCodeChallenge, PkceCodeVerifier};
 use regex::Regex;
@@ -13,12 +12,25 @@ const OKTA_AUTHORIZE_URL: &'static str =
     "https://ids.mlb.com/oauth2/aus1m088yK07noBfh356/v1/authorize";
 const OKTA_TOKEN_URL: &'static str = "https://ids.mlb.com/oauth2/aus1m088yK07noBfh356/v1/token";
 
-pub struct MlbSession {
-    client: reqwest::Client,
-    pub authn: Option<AuthnResponse>,
-    pub client_id: Option<String>,
-    pub okta_code: Option<String>,
-    pub okta_tokens: Option<OktaAuthResponse>,
+pub struct MlbSession<State> {
+    pub client: reqwest::Client,
+    pub state: State,
+}
+
+pub struct Unauthenticated;
+
+pub struct Authenticated {
+    pub authn: AuthnResponse,
+}
+
+pub struct Authorized {
+    pub client_id: String,
+    pub okta_code: String,
+    pub pkce_verifier: PkceCodeVerifier,
+}
+
+pub struct Tokens {
+    pub okta_tokens: OktaAuthResponse,
 }
 
 #[derive(Debug, Deserialize)]
@@ -31,46 +43,56 @@ pub struct AuthnResponse {
 
 #[derive(Debug, Deserialize)]
 pub struct OktaAuthResponse {
-    // pub token_type: String,
-    // pub expires_in: u32,
+    pub token_type: String,
+    pub expires_in: u32,
     pub access_token: String,
-    // pub scope: String,
-    // pub id_token: String,
+    pub scope: String,
+    pub id_token: String,
 }
 
-impl MlbSession {
+impl MlbSession<Unauthenticated> {
     pub fn new() -> anyhow::Result<Self> {
         let client = Client::builder().user_agent(USER_AGENT).build()?;
-        Ok(Self { client, authn: None, client_id: None, okta_code: None, okta_tokens: None })
+
+        Ok(Self {
+            client,
+            state: Unauthenticated,
+        })
     }
 
-    pub async fn login(&mut self) -> anyhow::Result<()> {
-        let cfg = AppConfig::load()?;
-
+    pub async fn authenticate(
+        self,
+        username: &str,
+        password: &str,
+    ) -> anyhow::Result<MlbSession<Authenticated>> {
         let req_body = json!({
-            "username": cfg.credentials.username,
-            "password": cfg.credentials.password,
+            "username": username,
+            "password": password,
             "options": {
                 "multiOptionalFactorEnroll": false,
                 "warnBeforePasswordExpired": true
             }
         });
 
-        let res = self.client
+        let res = self
+            .client
             .post("https://ids.mlb.com/api/v1/authn")
             .header("Content-Type", "application/json")
             .json(&req_body)
             .send()
             .await?;
 
-        // TODO: React accordingly if this fails. Know from personal experience that
-        // it generates a 403 error if you attempt to login while connected to a VPN.
-        let res_body: AuthnResponse = res.json().await?;
-        self.authn = Some(res_body);
-        Ok(())
-    }
+        let authn = res.json().await?;
 
-    pub async fn fetch_client_id(&mut self) -> anyhow::Result<()> {
+        Ok(MlbSession {
+            client: self.client,
+            state: Authenticated { authn },
+        })
+    }
+}
+
+impl MlbSession<Authenticated> {
+    async fn fetch_client_id(&self) -> anyhow::Result<String> {
         let res = self.client.get(MLB_OKTA_URL).send().await?;
         let res_body = res.text().await?;
 
@@ -78,22 +100,24 @@ impl MlbSession {
         let re = Regex::new(r#"production:\{clientId:"([^"]+)","#)?;
         if let Some(caps) = re.captures(&res_body) {
             if let Some(client_id) = caps.get(1) {
-                self.client_id = Some(client_id.as_str().to_string());
-                return Ok(())
-                // return Ok(client_id.as_str().to_string());
+                return Ok(client_id.as_str().to_string());
             }
         }
         anyhow::bail!("clientId not found in OKTA JS")
     }
 
-    pub async fn fetch_okta_code(&mut self, pkce_challenge: &PkceCodeChallenge) -> anyhow::Result<()> {
+    pub async fn fetch_okta_code(self) -> anyhow::Result<MlbSession<Authorized>> {
+        let (pkce_challenge, pkce_verifier) = PkceCodeChallenge::new_random_sha256();
         let state = CsrfToken::new_random_len(48);
         let nonce = CsrfToken::new_random_len(48);
 
-        let res = self.client
+        let client_id = self.fetch_client_id().await?;
+
+        let res = self
+            .client
             .get(OKTA_AUTHORIZE_URL)
             .query(&[
-                ("client_id", self.client_id.as_deref().expect("do a thing")),
+                ("client_id", client_id.as_str()),
                 ("response_type", "code"),
                 ("response_mode", "okta_post_message"),
                 ("scope", "openid profile email"),
@@ -102,7 +126,7 @@ impl MlbSession {
                 ("nonce", &nonce.secret()),
                 ("code_challenge", pkce_challenge.as_str()),
                 ("code_challenge_method", "S256"),
-                ("sessionToken", self.authn.as_ref().expect("authn must be set").session_token.as_str()),
+                ("sessionToken", self.state.authn.session_token.as_str()),
             ])
             .send()
             .await?;
@@ -113,44 +137,45 @@ impl MlbSession {
         let re = Regex::new(r#"data\.code\s*=\s*'([^']+)'"#)?;
         if let Some(caps) = re.captures(&res_body) {
             if let Some(m) = caps.get(1) {
-                self.okta_code = Some(unescape(m.as_str())?);
-                return Ok(());
+                let okta_code = unescape(m.as_str())?;
+                return Ok(MlbSession {
+                    client: self.client,
+                    state: Authorized {
+                        client_id,
+                        okta_code,
+                        pkce_verifier,
+                    },
+                });
             }
         }
         anyhow::bail!("Authorization code not found in okta_post_message response")
     }
+}
 
-    pub async fn fetch_okta_token(&mut self, pkce_verifier: &PkceCodeVerifier) -> anyhow::Result<()> {
-        let res = self.client
+impl MlbSession<Authorized> {
+    pub async fn exchange_tokens(self) -> anyhow::Result<MlbSession<Tokens>> {
+        let res = self
+            .client
             .post(OKTA_TOKEN_URL)
             .header("Accept", "application/json")
             .header("Content-Type", "application/x-www-form-urlencoded")
             .form(&[
-                ("client_id", self.client_id.as_deref().expect("do a thing")),
+                ("client_id", self.state.client_id.as_str()),
                 ("redirect_uri", "https://www.mlb.com/login"),
                 ("grant_type", "authorization_code"),
-                ("code_verifier", pkce_verifier.secret()),
-                ("code", self.okta_code.as_deref().expect("okta code must be set")),
+                ("code_verifier", self.state.pkce_verifier.secret()),
+                ("code", self.state.okta_code.as_str()),
             ])
             .send()
             .await?;
 
         let res_body: OktaAuthResponse = res.json().await?;
-        self.okta_tokens = Some(res_body);
-        Ok(())
-    }
 
-    pub async fn authenticate(&mut self) -> anyhow::Result<()> {
-        let (pkce_challenge, pkce_verifier) = oauth2::PkceCodeChallenge::new_random_sha256();
-
-        self.login().await?; // populates self.authn with a response containing a session token
-        self.fetch_client_id().await?; // populates self.client_id
-        self.fetch_okta_code(&pkce_challenge).await?; // populates self.okta_code
-        self.fetch_okta_token(&pkce_verifier).await?; // populates self.okta_tokens
-
-        let token = self.okta_tokens.as_ref().expect("fart frick heck!");
-
-        println!("{}", token.access_token);
-        Ok(())
+        Ok(MlbSession {
+            client: self.client,
+            state: Tokens {
+                okta_tokens: res_body,
+            },
+        })
     }
 }
